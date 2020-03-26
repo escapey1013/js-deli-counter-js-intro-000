31457a005e57d307a158f9edfa86df763c9d9ccc
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 17:35:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(array, name) {
   array.push(name);
   return `Welcome, ${name}. You are number ${array.length} in line.`
 }
+
+function nowServing(array) {
+  if (array.length > 0) {
+    var firstInLine = array.slice(0,1);
+    array.unshift();
+    return firstInLine;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
