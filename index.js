e07bf183639f7ef6af51a80526fa4a6252709910
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 17:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function nowServing(array) {
     return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(array) {
+  if (array.length === 0) {
+    return "The line is currently empty."
+  } else {
+      var message = "The line is currently: "
+      for (let i = 0; i < array.length; i++) {
+        var messageNew = `${message} ${i + 1}. ${array[i]}`;
+        var message = messageNew;
+      }
+      return message;
+    }
+  }
