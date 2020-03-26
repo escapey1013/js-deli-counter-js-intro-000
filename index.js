ded6420d35ac0e99f7f22afad72a6c05ae193f52
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 17:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function currentLine(array) {
   if (array.length === 0) {
     return "The line is currently empty."
   } else {
-      var message = "The line is currently:"
-      for (let i = 0; i < array.length; i++) {
-        var messageNew = `${message} ${i + 1}. ${array[i]},`;
+      var message = `The line is currently: 1. ${array[0]}`
+      for (let i = 1; i < array.length; i++) {
+        var messageNew = `${message}, ${i + 1}. ${array[i]}`;
         var message = messageNew;
       }
       return message;
